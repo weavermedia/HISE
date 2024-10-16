28849e7ba431ee082545005411978e7f6c137617
weavermedia <dan@Dweaver.media>
2024-10-15 18:45:45 -0700

    Remove HISE Standalone name

--- tools/auto_build/build_mac.sh
+++ tools/auto_build/build_mac.sh
@@ -12,11 +12,6 @@ chmod +x "tools/Projucer/Projucer.app/Contents/MacOS/Projucer"
 
 echo "Compiling Standalone App..."
 
-# Show build settings for Debug configuration
-echo "Debug Build settings:"
-xcodebuild -project "$standalone_folder/Builds/MacOSX/HISE Standalone.xcodeproj" -configuration Debug -showBuildSettings | grep BUILT_PRODUCTS_DIR
-
-# Actual build command
 xcodebuild -project "$standalone_folder/Builds/MacOSX/HISE Standalone.xcodeproj" -configuration Debug | xcpretty
 
 echo "Build completed successfully"
@@ -35,19 +30,12 @@ built_products_dir=$(echo "$built_products_dir" | sed 's/[[:space:]]*$//')
 
 echo "Final BUILT_PRODUCTS_DIR: $built_products_dir"
 
-# Check if the app exists with either name and rename it
-app_path_standalone="$built_products_dir/HISE Standalone.app"
+# Check if the app exists
 app_path_debug="$built_products_dir/HISE Debug.app"
 
-if [ -d "$app_path_standalone" ]; then
-    echo "Found HISE Standalone.app at $app_path_standalone"
-    mv "$app_path_standalone" "${app_path_standalone%/*}/HISE.app"
-    echo "HISE.app is ready for upload at ${app_path_standalone%/*}/HISE.app"
-elif [ -d "$app_path_debug" ]; then
-    echo "Found HISE Debug.app at $app_path_debug"
-    mv "$app_path_debug" "${app_path_debug%/*}/HISE.app"
-    echo "HISE.app is ready for upload at ${app_path_debug%/*}/HISE.app"
+if [ -d "$app_path_debug" ]; then
+  echo "HISE Debug.app is ready for upload at $app_path_debug"
 else
-    echo "Error: Built app not found"
-    exit 1
+  echo "Error: Built app not found"
+  exit 1
 fi
